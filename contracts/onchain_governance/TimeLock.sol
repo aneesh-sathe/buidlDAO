28236e81235e65ea9fc23f2850a8e777d42e3b09
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2023-02-18 23:31:02 +0530

    updated contracts

---
+++ contracts/onchain_governance/TimeLock.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifer : MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+
+contract TimeLock is TimelockController{
+    constructor(uint256 _minDelay, address[] memory _proposers, address[] memory _executors) TimelockController(_minDelay, _proposers, _executors, address(0)){}
+}
